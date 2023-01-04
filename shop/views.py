95a9d08e268b2601cf99b3224eb1c24b5c867fb8
TheKidKy <imsajadky@gmail.com>
2023-01-04 02:11:26 -0800

    created related products component

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.db.models import Count
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponseRedirect
+from utils.convertors import group_list
 
 from .models import Product, ProductCategory, ProductBrand, ProductComment
 from .forms import CommentForm
@@ -19,12 +20,17 @@ class products(ListView):
         context = super(products, self).get_context_data()
         return context
 
+
 class product_detail(DetailView):
     model = Product
     template_name = 'shop/product_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(product_detail, self).get_context_data(**kwargs)
+        current_product = self.object
+
+        context['related_products'] = group_list(
+            list(Product.objects.filter(brand_id=current_product.brand_id).exclude(pk=current_product.id).all()[:12]), 3)
         context['comment_form'] = CommentForm
 
         return context
@@ -42,14 +48,15 @@ def add_comment(request, id):
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
 def categories_component(request):
     product_categories = ProductCategory.objects.filter(is_active=True).annotate(product_count=Count('product'))
     product_brands = ProductBrand.objects.filter(is_active=True).annotate(product_count=Count('product'))
-    return render(request, 'shop/components/categories.html', context={'categories': product_categories, 'brands': product_brands})
+    return render(request, 'shop/components/categories.html',
+                  context={'categories': product_categories, 'brands': product_brands})
 
 
 def comments_component(request, id):
     product_id = id
     comments = ProductComment.objects.filter(product_id=product_id)
     return render(request, 'shop/components/comments_component.html', context={'comments': comments})
-
